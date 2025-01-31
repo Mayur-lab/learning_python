e9a086c96a2c50d9f6eba85247f48c744b5871ff
mayur walunj <mayur>
2025-01-31 17:25:32 +0530

    new file add

---
+++ Python Learn practice.py
@@ -0,0 +1,22 @@
+#  Day 1 python learn (Hello word )
+
+
+print("hello")
+print("walunj")
+
+print("walunj maruit walunj")
+
+
+print (" i am work in Layer One X cyptomarket company ")
+
+
+
+#  Day 2 python learn (keywords & identifiers)
+
+
+# str is identifier  
+str =  "my name is "
+
+print(str)
+
+
